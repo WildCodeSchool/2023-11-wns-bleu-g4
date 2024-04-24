666f811a0c66923ec59946f37a187eb4c93abec5
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-24 15:35:46 +0200

    feat: 🎸 Setup of tailwind variables
    Add of tailwind variables for colors and font family

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: [
-		"./src/app/**/*.{js,ts,tsx,mdx}",
-		"./src/components/**/*.{js,ts,tsx,mdx}",
-		"./src/pages/**/*.{js,ts,tsx,mdx}",
-	],
+	content: ["./src/**/**/*.{css,js,ts,tsx,mdx}"],
 	theme: {
 		extend: {
 			backgroundImage: {
@@ -13,6 +9,29 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+		},
+		colors: {
+			cactus: {
+				"50": "#E1E7E1",
+				"100": "#CAD6CC",
+				"200": "#D0DAD4",
+				"300": "#91B195",
+				"400": "#4F6F52",
+				"500": "#194D2E",
+				"600": "#324335",
+				"700": "#2C3B32",
+				"800": "#2a3b2c",
+				"900": "#233125",
+				"950": "#0B0F0B",
+			},
+			dark: "#140111",
+			light: "#F5EEE5",
+			accent: "#E66300",
+			babyGreen: "#B2E6C7",
+		},
+		fontFamily: {
+			header: ['"Poppins"', "sans-serif"],
+			sans: ['"Nunito"', "sans-serif"],
 		},
 	},
 	plugins: [],
