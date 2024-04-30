c5e1287ef4aad7c9a644b143b1303d5ff3b5bd82
chambrin <achambrin@outlook.fr>
2024-04-30 18:27:42 +0200

    refactor: 💡 Update subNav #22
    Box-shadow and ChevronDown rotate animation

--- frontend/src/features/navigation/SubNavbar.tsx
+++ frontend/src/features/navigation/SubNavbar.tsx
@@ -1,3 +1,5 @@
+import { useDisclosure, useOutsideClick } from "@chakra-ui/react";
+import { useRef, useState } from 'react';
 import {
     Menu,
     MenuButton,
@@ -7,6 +9,8 @@ import {
 } from '@chakra-ui/react';
 import {ChevronDownIcon, ShoppingCartIcon} from "@heroicons/react/16/solid";
 
+
+// data for the subnavbar
 const categories = {
     'Sea':  ['Option 1', 'Option 2', 'Option 3'],
     'Mountain': ['Option 1', 'Option 2', 'Option 3'],
@@ -14,26 +18,49 @@ const categories = {
 };
 
 export default function SubNavbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+    const ref = useRef<HTMLDivElement | null>(null);
+
+    useOutsideClick({
+        ref: ref,
+        handler: () => onClose()
+    });
+
+    const handleButtonClick = (index: number) => {
+        if (activeIndex === index) {
+            onClose();
+            setActiveIndex(null);
+        } else {
+            onOpen();
+            setActiveIndex(index);
+        }
+    };
+
     return (
-        <Flex className="h-8 w-full justify-between px-5 mb-5" >
+        <Flex className="h-8 shadow-md w-full justify-between px-5 pb-12 mb-5" ref={ref}>
             <Stack direction="row" spacing={4}>
                 {Object.entries(categories).map(([category, items], index) => (
-                    <Menu key={index}>
+                    <Menu key={index} isOpen={isOpen && activeIndex === index}>
                         <MenuButton as={Button}
                                     size="sm"
                                     variant="subNavButton"
                                     borderRadius='md'
                                     borderWidth='1px'
-                                    rightIcon={<ChevronDownIcon width={24} />}
-                                    py={4} // Ajoute un padding vertical de 2 unités
+                                    onClick={() => handleButtonClick(index)}
+                                    rightIcon={
+                                        <div
+                                            className={`transform transition-transform duration-300 ${isOpen && activeIndex === index ? 'rotate-custom' : ''}`} style={{ transform: isOpen && activeIndex === index ? 'rotate(-180deg)' : 'rotate(0)' }}>
+                                            <ChevronDownIcon width={24}/>
+                                        </div>
+                                    }
+                                    py={4}
                         >
                             {category}
                         </MenuButton>
-                        <MenuList bg="white">
+                        <MenuList>
                             {items.map((item, i) => (
                                 <MenuItem
-                                    bg="white"
-                                    color="black"
                                     key={i}>{item}</MenuItem>
                             ))}
                         </MenuList>
@@ -45,8 +72,7 @@ export default function SubNavbar() {
                     borderRadius="md"
                     borderWidth="1px"
                     variant="cartButton"
-                    leftIcon={<ShoppingCartIcon width={18}
-                    />}>
+                    leftIcon={<ShoppingCartIcon width={18} />}>
                 My basket
             </Button>
         </Flex>
