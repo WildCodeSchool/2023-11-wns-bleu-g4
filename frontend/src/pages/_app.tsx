ca1af6ceb7fc2a836857cbd0a9b6d19ab55fe94a
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-30 13:34:48 +0200

    feat: 🎸 Add of chakraProvider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import { ApolloProvider } from "@apollo/client";
 import client from "@/graphql/client";
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { theme } from "@/config/theme";
 
 
 function App({ Component, pageProps }: AppProps) {
 	return (
 		<ApolloProvider client={client}>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
+				<ColorModeScript initialColorMode={theme.globalTheme.initialColorMode} />
 				<Component {...pageProps} />
 			</ChakraProvider>
 		</ApolloProvider>
