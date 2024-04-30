c9ed51296cd96a2f9b52088cd234e1a9ee592b02
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-30 11:01:28 +0200

    fix: 🐛 Fix of variables location
    Move of colors & font variables in extend obj

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -4,34 +4,29 @@ const config: Config = {
 	content: ["./src/**/**/*.{css,js,ts,tsx,mdx}"],
 	theme: {
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				cactus: {
+					"50": "#E1E7E1",
+					"100": "#CAD6CC",
+					"200": "#D0DAD4",
+					"300": "#91B195",
+					"400": "#4F6F52",
+					"500": "#194D2E",
+					"600": "#324335",
+					"700": "#2C3B32",
+					"800": "#2a3b2c",
+					"900": "#233125",
+					"950": "#0B0F0B",
+				},
+				dark: "#140111",
+				light: "#F5EEE5",
+				accent: "#E66300",
+				babyGreen: "#B2E6C7",
 			},
-		},
-		colors: {
-			cactus: {
-				"50": "#E1E7E1",
-				"100": "#CAD6CC",
-				"200": "#D0DAD4",
-				"300": "#91B195",
-				"400": "#4F6F52",
-				"500": "#194D2E",
-				"600": "#324335",
-				"700": "#2C3B32",
-				"800": "#2a3b2c",
-				"900": "#233125",
-				"950": "#0B0F0B",
+			fontFamily: {
+				header: ['"Poppins"', "sans-serif"],
+				sans: ['"Nunito"', "sans-serif"],
 			},
-			dark: "#140111",
-			light: "#F5EEE5",
-			accent: "#E66300",
-			babyGreen: "#B2E6C7",
-		},
-		fontFamily: {
-			header: ['"Poppins"', "sans-serif"],
-			sans: ['"Nunito"', "sans-serif"],
 		},
 	},
 	plugins: [],
