d3c192a8523df596c29a26436e49561194bb1cfd
Jeremy Dohin <arkantik.pro@gmail.com>
2024-04-30 19:24:20 +0200

    feat: 🎸 Creation of Category class
    Creation of category class to be able to build Product resolver

---
+++ backend/src/entities/Category.ts
@@ -0,0 +1,26 @@
+import { Field, Int, ObjectType } from "type-graphql"
+import {
+	BaseEntity,
+	Column,
+	Entity,
+	OneToMany,
+	PrimaryGeneratedColumn,
+} from "typeorm"
+import Product from "./Product"
+
+@Entity()
+@ObjectType()
+export class Category extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	@Field(() => Int)
+	id: number
+
+	@Column()
+	@Field()
+	name: string
+
+	@OneToMany(() => Product, (product) => product.category)
+	products: Product[]
+}
+
+export default Category
