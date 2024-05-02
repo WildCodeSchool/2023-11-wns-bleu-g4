e8d45f620c1ae3026b8dab8f2feb09b5affed737
HpJkR <m.hertereau@gmail.com>
2024-05-02 13:48:29 +0200

    Create end to end test environment

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,7 +3,7 @@ import { User } from "./entities/User"
 import env from "./env"
 const { DB_USER, DB_PASS, DB_NAME, DB_PORT, DB_HOST } = env
 
-export const db = new DataSource({
+const db = new DataSource({
 	type: "postgres",
 	host: DB_HOST,
 	port: DB_PORT,
@@ -12,4 +12,15 @@ export const db = new DataSource({
 	database: DB_NAME,
 	entities: [User],
 	synchronize: true,
+	logging: env.NODE_ENV !== "test",
 })
+
+export async function clearDB() {
+	const entities = db.entityMetadatas
+	const tableNames = entities
+		.map((entity) => `"${entity.tableName}"`)
+		.join(", ")
+	await db.query(`TRUNCATE ${tableNames} RESTART IDENTITY CASCADE;`)
+}
+
+export default db
