4830406caf0a52f3c76e4d3351c257ade2d8007c
Jeremy Dohin <arkantik.pro@gmail.com>
2024-05-02 16:28:28 +0200

    refactor: 💡 Update with SubCategory Resolver

--- backend/src/schema.ts
+++ backend/src/schema.ts
@@ -3,8 +3,14 @@ import { authChecker } from "./auth"
 import UserResolver from "./resolvers/UserResolver"
 import ProductResolver from "./resolvers/ProductResolver"
 import { ReviewResolver } from "./resolvers/ReviewResolver"
+import SubCategoryResolver from "./resolvers/SubCategoryResolver"
 
 export default buildSchema({
-	resolvers: [UserResolver, ProductResolver, ReviewResolver],
+	resolvers: [
+		UserResolver,
+		ReviewResolver,
+		ProductResolver,
+		SubCategoryResolver,
+	],
 	authChecker,
 })
