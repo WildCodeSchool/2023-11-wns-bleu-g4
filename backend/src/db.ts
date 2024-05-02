cf24306f158d4c171c5ae6f12394983aa5a085e4
Jeremy Dohin <arkantik.pro@gmail.com>
2024-05-02 15:10:50 +0200

    refactor: 💡 Add of entities
    Add of Review, Product & Category entities

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,6 +1,9 @@
 import { DataSource } from "typeorm"
 import { User } from "./entities/User"
 import env from "./env"
+import Review from "./entities/Review"
+import Product from "./entities/Product"
+import Category from "./entities/Category"
 const { DB_USER, DB_PASS, DB_NAME, DB_PORT, DB_HOST } = env
 
 export const db = new DataSource({
@@ -10,6 +13,6 @@ export const db = new DataSource({
 	username: DB_USER,
 	password: DB_PASS,
 	database: DB_NAME,
-	entities: [User],
+	entities: [User, Review, Product, Category],
 	synchronize: true,
 })
