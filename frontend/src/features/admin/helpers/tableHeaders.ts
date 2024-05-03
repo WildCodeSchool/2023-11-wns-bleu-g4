7228e7c87de93923741e4e6b83092507eed6b8d8
Jeremy Dohin <arkantik.pro@gmail.com>
2024-05-03 20:06:26 +0200

    feat: 🎸 Creation of helpers for admin tables

---
+++ frontend/src/features/admin/helpers/tableHeaders.ts
@@ -0,0 +1,13 @@
+import { menuItems } from "../types";
+
+const orderTableHeaders: menuItems[] = [
+  { id: 1, name: "order number" },
+  { id: 2, name: "customer name" },
+  { id: 3, name: "customer address" },
+  { id: 4, name: "agency" },
+  { id: 5, name: "from" },
+  { id: 6, name: "to" },
+  { id: 7, name: "order details" },
+];
+
+export default orderTableHeaders;
