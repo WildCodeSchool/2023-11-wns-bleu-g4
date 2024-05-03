024ae5fb04dc4d87ef00068d8eaeb575c79de837
HpJkR <m.hertereau@gmail.com>
2024-05-03 11:07:33 +0200

    create bento grid component

--- frontend/src/config/theme.tsx
+++ frontend/src/config/theme.tsx
@@ -14,8 +14,8 @@ export const theme = extendTheme({
     Drawer: drawerTheme,
   },
   fonts: {
-    heading: "var(--font-rubik)",
-    body: "var(--font-rubik)",
+    heading: "Poppins, sans-serif",
+    body: "Nunito, sans-serif",
   },
   globalTheme: {
     initialColorMode: "light",
@@ -44,6 +44,10 @@ export const theme = extendTheme({
     footerBgLight: "#F2F2F2",
     footerBgDark: "#0C0C0B",
     cardBgLight: "#f5eee5e6",
+    primary: "#194D2E",
+    secondary: "#4F6F52",
+    primaryDark: "#B2E6C7",
+    secondaryDark: "#91B195",
   },
   styles: {
     global: (props: any) => ({
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,10 @@
+import BentoGrid from "@/features/home/infos/BentoGrid";
 import Layout from "@/layouts/Layout";
 
 export default function Home() {
   return (
     <Layout pageTitle="Home">
-      <p>This is home</p>
+      <BentoGrid />
     </Layout>
   );
 }
