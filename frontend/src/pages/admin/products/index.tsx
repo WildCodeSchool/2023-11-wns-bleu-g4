6f300a4b97008a03d3bc34b9069ede326cb96f4d
Jeremy Dohin <arkantik.pro@gmail.com>
2024-06-03 16:12:25 +0200

    feat: 🎸 Add of table components and logic

--- frontend/src/pages/admin/products/index.tsx
+++ frontend/src/pages/admin/products/index.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import TableFooter from "@/features/admin/table/TableFooter";
 import LayoutAdmin from "@/layouts/LayoutAdmin";
+import data from "@/features/admin/helpers/dummyProducts";
+import ProductTableBody from "@/features/admin/table/ProductTableBody";
 
 export default function Products() {
+  const [sortedData, setSortedData] = useState<any[]>(data);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsPerPage = 16;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + Math.min(itemsPerPage, data?.length ?? 0);
+  const currentOrders = sortedData.slice(startIndex, endIndex);
+
   return (
-    <LayoutAdmin>
+    <LayoutAdmin pageTitle="Product list">
       <h1>Product list</h1>
+      <div className="overflow-x-auto">
+        <ProductTableBody data={currentOrders} />
+      </div>
+      <TableFooter
+        data={sortedData}
+        startIndex={startIndex}
+        endIndex={endIndex}
+        currentPage={currentPage}
+        itemsPerPage={itemsPerPage}
+        setCurrentPage={setCurrentPage}
+      />
     </LayoutAdmin>
   );
 }
