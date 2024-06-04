51725413fca41628ce9d2db8578f3eea94db2ad1
xakaz <loic.hernandez@sfr.fr>
2024-06-04 13:23:18 +0200

    Add types in types.ts

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,3 +1,4 @@
+import { InputType, Field, Int } from "type-graphql";
 import { User } from "./entities/User"
 import express from "express"
 
@@ -5,4 +6,20 @@ export interface ContextType {
 	req: express.Request
 	res: express.Response
 	currentUser?: User
+}
+
+@InputType()
+export class ObjectId {
+	@Field(() => Int)
+	id!: number;
+}
+@InputType()
+export class UserId {
+	@Field(() => Int)
+	id!: number;
+}
+@InputType()
+export class AgencyId {
+	@Field(() => Int)
+	id!: number;
 }
