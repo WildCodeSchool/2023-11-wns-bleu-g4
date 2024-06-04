1fa3ddd0c609175f16a2e9f890ee5ecc198364e5
xakaz <loic.hernandez@sfr.fr>
2024-06-04 18:01:25 +0200

    add queries and mutations

--- backend/src/resolvers/BookingItemResolver.ts
+++ backend/src/resolvers/BookingItemResolver.ts
@@ -11,7 +11,7 @@ import {
 import Agency, { NewAgencyInput, UpdateAgencyInput } from "../entities/Agency"
 import { UserRole } from "../entities/User"
 import { Context } from "../utils"
-import { BookingItem } from "../entities/BookingItem"
+import { BookingItem, UpdateBookingItemInput } from "../entities/BookingItem"
 
 @Resolver()
 class BookingItemResolver {
@@ -20,18 +20,48 @@ class BookingItemResolver {
         return BookingItem.find()
     }
 
-    @Query(() => [BookingItem])
-    async getBookingItemsBy(
-        @Arg ("bookingId") bookingId: number
+    /** En attendant la validation de la PR de Booking */
+    // @Query(() => [BookingItem])
+    // async getBookingItemsBy(
+    //     @Arg ("bookingId") bookingId: number
+    // ) {
+    //     const items = await BookingItem.find({
+    //         where : {booking : {
+    //             id : bookingId
+    //         }},
+    //         relation : {booking : true, product:true}
+    //     })
+    // }
+
+    @Mutation(() => String)
+    async updateBookingItem(
+        @Arg("bookingItemId") id: number,
+        @Arg("data", { validate: true }) data: UpdateBookingItemInput,
+        @Ctx() ctx: Context
     ) {
-        const items = await BookingItem.find({
-            where : {booking : {
-                id : bookingId
-            }},
-            relation : {booking : true, product:true}
-        })
+        if (!ctx.currentUser) throw new GraphQLError("Not authenticated")
+
+        const itemToUpdate = await BookingItem.findOne({ where: { id } })
+        if (!itemToUpdate) throw new GraphQLError("Item not found")
+
+        Object.assign(itemToUpdate, data)
+
+        await itemToUpdate.save()
+
+        return `${itemToUpdate.name} updated`
+
     }
 
+    @Mutation(() => String)
+    async deleteBookingItem(@Arg("bookingItemId") id: number, @Ctx() ctx: Context) {
+        if (!ctx.currentUser) throw new GraphQLError("Not authenticated")
+
+        const itemToDelete = await BookingItem.findOne({ where: { id } })
+        if (!itemToDelete) throw new GraphQLError("Item not found")
+
+        await itemToDelete.remove()
+        return `${itemToDelete.name} deleted`
+    }
 
 }
 
