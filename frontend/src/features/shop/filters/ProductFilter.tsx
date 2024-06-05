02f0a12823c907bbf95394cf60c776a46828ec76
Chambrin <achambrin@outlook.fr>
2024-06-05 17:21:56 +0200

    filter layout

--- frontend/src/features/shop/filters/ProductFilter.tsx
+++ frontend/src/features/shop/filters/ProductFilter.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Flex, Text, Button, CheckboxGroup, Checkbox, Stack } from "@chakra-ui/react";
 
 const ProductFilter: React.FC = () => {
+    // Liste des catégories
+    const categories = ['Catégorie 1', 'Catégorie 2', 'Catégorie 3', 'Catégorie 4'];
+
     return (
-        <div>
-           Product Filter
-        </div>
+        <Flex direction="column" justifyContent="space-between" alignItems="start" padding="1rem">
+            <Flex justifyContent="space-between" alignItems="center" width="100%">
+                <Text fontSize="xl" fontWeight="bold">Filter</Text>
+                <Text colorScheme="teal" cursor="pointer">Clear Filter</Text>
+            </Flex>
+            <Text fontSize="xl" fontWeight="bold" mt="2">Categorie</Text>
+            <CheckboxGroup colorScheme="teal" defaultValue={[]}>
+                <Stack spacing={3} mt="2">
+                    {categories.map((category, index) => (
+                        <Checkbox key={index} value={category} borderRadius="full">{category}</Checkbox>
+                    ))}
+                </Stack>
+            </CheckboxGroup>
+        </Flex>
     );
 }
 
