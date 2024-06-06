e61caa282fbd8fd2305298230dbc07133277eca2
Jeremy Dohin <arkantik.pro@gmail.com>
2024-06-06 13:07:13 +0200

    style: 💄 Prettier workflow

--- frontend/src/pages/admin/products/[id]/index.tsx
+++ frontend/src/pages/admin/products/[id]/index.tsx
@@ -5,27 +5,27 @@ import LayoutAdmin from "@/layouts/LayoutAdmin";
 import { useRouter } from "next/router";
 
 export default function ProductPage() {
-    const { query } = useRouter();
+  const { query } = useRouter();
 
-    const product = {
-        id: 1,
-        reference: "REF001",
-        name: "Product 1",
-        description:
-            "This is a product with a long description that is around 200 characters. It provides detailed information about the product, including its features, benefits, and usage instructions. Customers can read this description to make an informed decision before purchasing the product.",
-        brand: "Brand 1",
-        price: 10.99,
-        thumbnail: "https://www.squid-surfboards.com/wp-content/uploads/2020/12/RASTA-copie.png",
-        category: "Category 1",
-        stocks: productStocks,
-    };
+  const product = {
+    id: 1,
+    reference: "REF001",
+    name: "Product 1",
+    description:
+      "This is a product with a long description that is around 200 characters. It provides detailed information about the product, including its features, benefits, and usage instructions. Customers can read this description to make an informed decision before purchasing the product.",
+    brand: "Brand 1",
+    price: 10.99,
+    thumbnail: "https://www.squid-surfboards.com/wp-content/uploads/2020/12/RASTA-copie.png",
+    category: "Category 1",
+    stocks: productStocks,
+  };
 
-    return (
-        <LayoutAdmin pageTitle="Product">
-            <section className="flex gap-8">
-                <ProductDetails product={product} />
-                <ProductStocks product={product} />
-            </section>
-        </LayoutAdmin>
-    );
+  return (
+    <LayoutAdmin pageTitle="Product">
+      <section className="flex gap-8">
+        <ProductDetails product={product} />
+        <ProductStocks product={product} />
+      </section>
+    </LayoutAdmin>
+  );
 }
