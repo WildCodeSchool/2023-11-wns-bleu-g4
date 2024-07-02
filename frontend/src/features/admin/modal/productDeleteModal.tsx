94786d72ec975edd6982911c94e67a19899d3907
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-02 11:37:35 +0200

    feat: 🎸 Add of delete logic

--- frontend/src/features/admin/modal/productDeleteModal.tsx
+++ frontend/src/features/admin/modal/productDeleteModal.tsx
@@ -10,18 +10,20 @@ import {
 } from "@chakra-ui/react";
 import { ProductModalProps } from "../types";
 
-export default function ProductDeleteModal({ isOpen, onClose, product, variant }: ProductModalProps) {
+export default function ProductDeleteModal({ isOpen, onClose, product: { id, name }, variant, handleDeleteProduct }: ProductModalProps) {
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} variant={variant} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Delete {product.name}</ModalHeader>
+        <ModalHeader>Delete {name}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody pb={6}>Are you sure you want to delete {product.name}? This action cannot be undone.</ModalBody>
+        <ModalBody pb={6}>Are you sure you want to delete <b>{name}</b>? This action cannot be undone.</ModalBody>
 
         <ModalFooter>
           <Button onClick={onClose}>Cancel</Button>
-          <Button colorScheme="red" ml={3} type="submit">
+          <Button colorScheme="red" ml={3} type="submit"
+            onClick={() => { handleDeleteProduct && handleDeleteProduct(id) }}>
             Delete
           </Button>
         </ModalFooter>
