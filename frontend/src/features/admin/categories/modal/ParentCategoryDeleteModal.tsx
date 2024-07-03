49d17233694fadf7733755d738874aa682d5de36
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-03 17:33:27 +0200

    feat: 🎸 Creation of modal to delete parent category

---
+++ frontend/src/features/admin/categories/modal/ParentCategoryDeleteModal.tsx
@@ -0,0 +1,32 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { CategoryModalProps } from "../types";
+
+export default function ParentCategoryDeleteModal({ isOpen, onClose, parentCategory, variant, handleDelete }: CategoryModalProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} variant={variant} isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Delete {parentCategory?.name}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>Are you sure you want to delete <b>{parentCategory?.name} parent category</b>? This action cannot be undone.</ModalBody>
+
+        <ModalFooter>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button colorScheme="red" ml={3} type="submit"
+            onClick={() => { handleDelete && handleDelete(parentCategory?.id!) }}>
+            Delete
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
