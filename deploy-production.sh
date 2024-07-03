809238d32e67d07b75cf2f6ec8f4cee5e6329981
xakaz <loic.hernandez@sfr.fr>
2024-07-03 16:39:09 +0200

    new .env file

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-set -a
-source .env.production
-set +a
-git fetch origin
+# #!/bin/bash
+# set -a
+# source .env.production
+# set +a
+# git fetch origin
 
-git reset --hard origin/staging
-docker compose -f docker-compose.production.yml down
-docker compose -f docker-compose.production.yml pull
-docker compose -f docker-compose.production.yml up -d
+# git reset --hard origin/staging
+# docker compose -f docker-compose.production.yml down
+# docker compose -f docker-compose.production.yml pull
+# docker compose -f docker-compose.production.yml up -d
 
 
 
-# #!/bin/sh
-# git fetch origin && git reset --hard origin/main && git clean -f -d && \
-# docker compose -f docker-compose.production.yml down && \
-# docker compose -f docker-compose.production.yml pull && \
-# docker compose -f docker-compose.production.yml --env-file .env.production up -d;
+#!/bin/sh
+git fetch origin && git reset --hard origin/staging && git clean -f -d && \
+docker compose -f docker-compose.production.yml down && \
+docker compose -f docker-compose.production.yml pull && \
+docker compose -f docker-compose.production.yml --env-file .env.production up -d;
