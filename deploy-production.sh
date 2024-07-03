b0afb3e5ff8db571bec4ae7f643480b441cf935a
xakaz <loic.hernandez@sfr.fr>
2024-07-03 16:34:49 +0200

    new changes

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
-git fetch origin && git reset --hard origin/main && git clean -f -d && \
-docker compose -f docker-compose.production.yml down && \
-docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file .env.production up -d;
+#!/bin/bash
+set -a
+source .env.production
+set +a
+git fetch origin
+
+git reset --hard origin/staging
+docker compose -f docker-compose.production.yml down
+docker compose -f docker-compose.production.yml pull
+docker compose -f docker-compose.production.yml up -d
+
+
+
+# #!/bin/sh
+# git fetch origin && git reset --hard origin/main && git clean -f -d && \
+# docker compose -f docker-compose.production.yml down && \
+# docker compose -f docker-compose.production.yml pull && \
+# docker compose -f docker-compose.production.yml --env-file .env.production up -d;
