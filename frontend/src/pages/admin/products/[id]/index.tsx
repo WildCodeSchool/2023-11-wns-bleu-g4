9c1f6c31f4d29a1f3498122ab7c7e26ee3af1741
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-03 14:08:17 +0200

    refactor: 💡 Add of product picture component

--- frontend/src/pages/admin/products/[id]/index.tsx
+++ frontend/src/pages/admin/products/[id]/index.tsx
@@ -1,5 +1,6 @@
 import { productStocks } from "@/features/admin/helpers/dummyProducts";
 import ProductDetails from "@/features/admin/product/ProductDetails";
+import ProductPictures from "@/features/admin/product/ProductPictures";
 import ProductStocks from "@/features/admin/product/ProductStocks";
 import { useGetProductByIdQuery } from "@/graphql/Product/generated/getProductById.generated";
 import LayoutAdmin from "@/layouts/LayoutAdmin";
@@ -13,8 +14,10 @@ export default function ProductPage() {
 
   return (
     <LayoutAdmin pageTitle="Product">
+      <h2>Informations</h2>
       <section className="flex gap-8">
         <ProductDetails product={product} />
+        <ProductPictures product={product} />
         {/* <ProductStocks product={product} /> */}
       </section>
     </LayoutAdmin>
