fdb86b06046d3b07256f693fce0e09ba9d59c9c7
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-03 17:32:25 +0200

    fix: 🐛 Fix of parentCategory relation

--- backend/src/resolvers/CategoryResolver.ts
+++ backend/src/resolvers/CategoryResolver.ts
@@ -1,20 +1,9 @@
-import {
-	Resolver,
-	Query,
-	Arg,
-	Mutation,
-	Int,
-	Ctx,
-	Authorized,
-} from "type-graphql"
+import { Resolver, Query, Arg, Mutation, Int, Ctx, Authorized } from "type-graphql"
 import { GraphQLError } from "graphql"
 import { ILike } from "typeorm"
 import { Context } from "../utils"
 import { UserRole } from "../entities/User"
-import Category, {
-	NewCategoryInput,
-	UpdateCategoryInput,
-} from "../entities/Category"
+import Category, { NewCategoryInput, UpdateCategoryInput } from "../entities/Category"
 
 @Resolver()
 class CategoryResolver {
@@ -25,11 +14,11 @@ class CategoryResolver {
 		@Arg("name", { nullable: true }) name?: string
 	) {
 		return Category.find({
-			relations: { products: true, parentCategories: true },
+			relations: { products: true, parentCategory: true },
 			where: {
 				name: name ? ILike(`%${name}%`) : undefined,
 				products: { id: productId },
-				parentCategories: { id: subCategoryId },
+				parentCategory: { id: subCategoryId },
 			},
 		})
 	}
@@ -38,7 +27,7 @@ class CategoryResolver {
 	async getCategoryById(@Arg("categoryId", () => Int) id: number) {
 		const category = await Category.findOne({
 			where: { id },
-			relations: { products: true, parentCategories: true },
+			relations: { products: true, parentCategory: true },
 		})
 		if (!category) throw new GraphQLError("Not found")
 		return category
@@ -46,21 +35,17 @@ class CategoryResolver {
 
 	@Authorized([UserRole.ADMIN])
 	@Mutation(() => Category)
-	async createCategory(
-		@Arg("data", { validate: true }) data: NewCategoryInput,
-		@Ctx() ctx: Context
-	) {
+	async createCategory(@Arg("data", { validate: true }) data: NewCategoryInput, @Ctx() ctx: Context) {
 		if (!ctx.currentUser) throw new GraphQLError("Not authenticated")
 		const newCategory = new Category()
 
-		if (ctx.currentUser.role !== UserRole.ADMIN)
-			throw new GraphQLError("Not authorized")
+		if (ctx.currentUser.role !== UserRole.ADMIN) throw new GraphQLError("Not authorized")
 		Object.assign(newCategory, data)
 
 		const { id } = await newCategory.save()
 		return Category.findOne({
 			where: { id },
-			relations: { products: true, parentCategories: true },
+			relations: { products: true, parentCategory: true },
 		})
 	}
 
@@ -75,14 +60,13 @@ class CategoryResolver {
 		const categoryToUpdate = await Category.findOne({ where: { id } })
 		if (!categoryToUpdate) throw new GraphQLError("Category not found")
 
-		if (ctx.currentUser.role !== UserRole.ADMIN)
-			throw new GraphQLError("Not authorized")
+		if (ctx.currentUser.role !== UserRole.ADMIN) throw new GraphQLError("Not authorized")
 		await Object.assign(categoryToUpdate, data)
 
 		await categoryToUpdate.save()
 		return Category.findOne({
 			where: { id },
-			relations: { products: true, parentCategories: true },
+			relations: { products: true, parentCategory: true },
 		})
 	}
 
@@ -93,8 +77,7 @@ class CategoryResolver {
 		const categoryToDelete = await Category.findOne({ where: { id } })
 		if (!categoryToDelete) throw new GraphQLError("Category not found")
 
-		if (ctx.currentUser.role !== UserRole.ADMIN)
-			throw new GraphQLError("Not authorized")
+		if (ctx.currentUser.role !== UserRole.ADMIN) throw new GraphQLError("Not authorized")
 
 		await categoryToDelete.remove()
 		return "Category deleted"
