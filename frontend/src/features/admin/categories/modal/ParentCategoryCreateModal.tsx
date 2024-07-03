56da2a6d83a7d3c4fd2b628ead5e9ccf46eb9422
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-03 17:33:10 +0200

    feat: 🎸 Creation of modal to create parent category

---
+++ frontend/src/features/admin/categories/modal/ParentCategoryCreateModal.tsx
@@ -0,0 +1,78 @@
+"use client";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { CategoryModalProps } from "../types";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useCreateParentCategoryMutation } from "@/graphql/ParentCategory/generated/createParentCategory.generated";
+
+
+export default function ParentCategoryCreateModal({ isOpen, onClose, refetch }: CategoryModalProps) {
+  const [createParentCategory] = useCreateParentCategoryMutation();
+  const [formData, setFormData] = useState({ name: "" });
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await createParentCategory({ variables: { data: formData } });
+      refetch && refetch();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} variant="darkOverlayStyle" isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Add a new product</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired>
+              <FormLabel mb={1} htmlFor="name">
+                Name
+              </FormLabel>
+              <Input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+            <ModalFooter paddingInline={0} pb={0} pt={8}>
+              <Button onClick={onClose}>Cancel</Button>
+              <Button colorScheme="blue" ml={3} type="submit">
+                Create
+              </Button>
+            </ModalFooter>
+          </form>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
