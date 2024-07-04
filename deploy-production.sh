efc42ede43052ab961493b3f429b931d1fc723ad
xakaz <loic.hernandez@sfr.fr>
2024-07-04 11:40:12 +0200

    enable uploads

--- deploy-production.sh
+++ deploy-production.sh
@@ -1,18 +1,6 @@
-# #!/bin/bash
-# set -a
-# source .env.production
-# set +a
-# git fetch origin
-
-# git reset --hard origin/staging
-# docker compose -f docker-compose.production.yml down
-# docker compose -f docker-compose.production.yml pull
-# docker compose -f docker-compose.production.yml up -d
-
-
-
 #!/bin/sh
 git fetch origin && git reset --hard origin/staging && git clean -f -d && \
 docker compose -f docker-compose.production.yml down && \
 docker compose -f docker-compose.production.yml pull && \
-docker compose -f docker-compose.production.yml --env-file .env.production up -d;
+docker compose -f docker-compose.production.yml --env-file .env.production up -d --build;
+# docker compose -f docker-compose.production.yml --env-file .env.production up -d;
