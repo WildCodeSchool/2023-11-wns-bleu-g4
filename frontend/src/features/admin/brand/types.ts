0ddc9ad2b768e870215983a397e878978bd6fc91
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-09 17:34:28 +0200

    feat: 🎸 Creation of type file for brand

---
+++ frontend/src/features/admin/brand/types.ts
@@ -0,0 +1,12 @@
+export interface Brand {
+  id?: number;
+  name: string;
+  logo: string;
+}
+
+export interface BrandModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  brand: Brand | undefined;
+  handleDelete: (id: number) => void;
+}
