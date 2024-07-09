eef459092e112e744ed506cbe1b26f64d1a3c0b3
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-09 17:34:39 +0200

    feat: 🎸 Creation of delete brand modal

---
+++ frontend/src/features/admin/brand/BrandDeleteModal.tsx
@@ -0,0 +1,39 @@
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { BrandModalProps } from "./types";
+
+export default function CategoryDeleteModal({ isOpen, onClose, brand, handleDelete }: BrandModalProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} variant="darkOverlayStyle" isCentered>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Delete {brand?.name}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          Are you sure you want to delete <b>{brand?.name} brand</b>? This action cannot be undone.
+        </ModalBody>
+        <ModalFooter>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button
+            colorScheme="red"
+            ml={3}
+            type="submit"
+            onClick={() => {
+              handleDelete && handleDelete(brand?.id!);
+            }}
+          >
+            Delete
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
