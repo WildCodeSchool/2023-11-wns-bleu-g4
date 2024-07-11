c6bd008ec1e65aed8bd77b4e233b11f334de74ab
Jeremy Dohin <arkantik.pro@gmail.com>
2024-07-11 14:54:41 +0200

    refactor: 💡 Move of interfaces to order type file

--- frontend/src/features/admin/orders/types.ts
+++ frontend/src/features/admin/orders/types.ts
@@ -18,3 +18,18 @@ export interface BookingItem {
   product: { name: string; price: number };
   productCode: { id: number };
 }
+
+export interface OrderModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  order: Order;
+  handleCancel?: (id: number) => void;
+}
+
+export interface OrderTableBodyProps {
+  data: any[];
+  refetch?: () => void;
+  sortColumnName: string | null;
+  sortOrder: "asc" | "desc" | null;
+  handleDateSort: (columnName: string) => void;
+}
