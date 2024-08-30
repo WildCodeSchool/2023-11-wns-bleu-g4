9e39bc001351c7b6c9415781117d9dcdf94f4304
HpJkR <m.hertereau@gmail.com>
2024-08-30 09:23:39 +0200

    Create Review manager and user context

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -13,6 +13,7 @@ import "react-toastify/dist/ReactToastify.css";
 import {UserProvider} from "@/context/UserDataContext";
 import "@/styles/globals.css";
 import "@/styles/toast.css";
+import {UserProvider} from "@/context/UserDataContext";
 
 function App({Component, pageProps}: AppProps) {
   return (
