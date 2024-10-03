16b25d4c2d840339377ac1dde880ea027f68538e
Loïc Hernandez <xakaz31330@gmail.com>
2024-10-03 14:43:55 +0200

    add gateway port in deploy dev script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,4 +2,4 @@
 git fetch origin && git reset --hard origin/dev && git clean -f -d && \
 docker compose -f docker-compose.dev.yml down && \
 docker compose -f docker-compose.dev.yml pull && \
-docker compose -f docker-compose.dev.yml --env-file .env.production up -d --build;
+GATEWAY_PORT=81 docker compose -f docker-compose.dev.yml --env-file .env.production up -d --build;
