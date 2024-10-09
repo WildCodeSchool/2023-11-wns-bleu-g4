66cddac158254947e01f8d53092b62635db263fa
HpJkR <m.hertereau@gmail.com>
2024-10-09 13:52:27 +0200

    update basket ui and fix array items

--- frontend/src/context/BookingDataContext.tsx
+++ frontend/src/context/BookingDataContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 interface ProfileData {
   name: string;
